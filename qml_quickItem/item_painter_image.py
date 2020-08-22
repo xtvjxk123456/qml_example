c3ffbcf9895a4547223ade9343fb1d01ad15cfe0
xtvjxk <xtvjxk123456maya@163.com>
2020-08-22 23:55:38 +0800

    add 这个例子使用view启动，不支持window作为root,内容缩放需要设置

--- qml_quickItem/item_painter_image.py
+++ qml_quickItem/item_painter_image.py
@@ -17,11 +17,11 @@ class QuickItem(QtQuick.QQuickPaintedItem):
         painter.save()
         test_rect = QtCore.QRectF()
         test_rect.setTopLeft(rect.topLeft() + QtCore.QPointF(rect.width() / 6, rect.height() / 6).toPoint())
-        test_rect.setBottomRight(rect.bottomRight() - QtCore.QPointF(rect.width() / 6, rect.height()  / 6).toPoint())
+        test_rect.setBottomRight(rect.bottomRight() - QtCore.QPointF(rect.width() / 6, rect.height() / 6).toPoint())
         pen = QtGui.QPen(QtGui.QBrush(QtCore.Qt.red), 2)
         painter.setPen(pen)
         painter.drawRoundRect(test_rect, 10)
-        painter.drawRoundRect(rect,10)
+        painter.drawRoundRect(rect, 10)
         painter.restore()
 
 
@@ -29,8 +29,17 @@ if __name__ == "__main__":
     app = QtGui.QGuiApplication(sys.argv[1:])
     QtQml.qmlRegisterType(QuickItem, "Demo", 1, 0, "PaintItem")
     # 类型不能小写开头，要和qml中的类似，大写开头
-    engine = QtQml.QQmlApplicationEngine()
-    engine.addImportPath(os.path.dirname(__file__))
-    engine.load('resource\\main.qml')
+    # -------------------------------------------主体是window
+    # engine = QtQml.QQmlApplicationEngine()
+    # engine.addImportPath(os.path.dirname(__file__))
+    # engine.load('resource\\main.qml')
+    # --------------------------------------------------------
+    view = QtQuick.QQuickView()
+    # view 不支持window 作为root
+    view.setResizeMode(QtQuick.QQuickView.SizeRootObjectToView)
+    view.engine().addImportPath(os.path.dirname(__file__))
+    view.setSource('resource\\main.qml')
+    view.show()
+    # ----------------------------------------------------------
 
     sys.exit(app.exec_())
--- qml_quickItem/resource/main.qml
+++ qml_quickItem/resource/main.qml
@@ -1,12 +1,10 @@
 import QtQuick 2.11
-import QtQuick.Window 2.0
 import Demo 1.0
 
-Window {
+Item{
     visible: true
     width: 640
     height: 480
-    title: qsTr("Hello Item")
 
     PaintItem {
         anchors.fill: parent
