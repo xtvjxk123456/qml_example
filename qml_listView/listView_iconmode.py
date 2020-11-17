7ab63d7febb47555d3f8aaa55103da5ab1518e35
xtvjxk <xtvjxk123456maya@163.com>
2020-11-17 23:00:05 +0800

    添加基本文件

---
+++ qml_listView/listView_iconmode.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+from PySide2 import QtWidgets, QtCore, QtGui, QtQuick, QtQml
+import sys
+import os
+import pprint
+
+
+def printError(status, view):
+    if status == QtQuick.QQuickView.Error:
+        pprint.pprint(view.errors())
+
+
+if __name__ == "__main__":
+    app = QtGui.QGuiApplication(sys.argv[1:])
+    view = QtQuick.QQuickView()
+    view.statusChanged.connect(lambda x: printError(x, view))
+    view.setSource("resource/listview_iconMode.qml")
+    view.show()
+    sys.exit(app.exec_())
+
+
