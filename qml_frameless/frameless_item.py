a5b823215971d5ad406d5a32e23cb16f52e851ff
xtvjxk <xtvjxk123456maya@163.com>
2020-11-18 00:01:54 +0800

    不用window,只用item,需要修改quickview

---
+++ qml_frameless/frameless_item.py
@@ -0,0 +1,22 @@
+from PySide2 import QtWidgets, QtCore, QtGui, QtQuick, QtQml
+import sys
+import os
+import pprint
+
+
+def printError(status, view):
+    if status == QtQuick.QQuickView.Error:
+        pprint.pprint(view.errors())
+
+
+if __name__ == "__main__":
+    app = QtGui.QGuiApplication(sys.argv[1:])
+    view = QtQuick.QQuickView()
+    view.statusChanged.connect(lambda x: printError(x, view))
+    view.setResizeMode(QtQuick.QQuickView.SizeRootObjectToView)
+    view.setFlags(QtCore.Qt.FramelessWindowHint)
+    view.setSource("resource/frameless_item.qml")
+    view.show()
+    # engine = QtQml.QQmlApplicationEngine()
+    # engine.load("resource/frameless.qml")
+    sys.exit(app.exec_())
---
+++ qml_frameless/resource/frameless_item.qml
@@ -0,0 +1,14 @@
+import QtQuick 2.0
+
+Rectangle {
+id: myWindow
+width: 500; height: 500
+color: "yellow"
+
+Rectangle {
+anchors.centerIn: parent
+width: 200; height: 200
+
+color: "red"
+}
+}
